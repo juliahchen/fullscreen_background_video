0de59321fc8bc4b0236d5ed6b1f0c131b0a4a607
Julia Chen <julia@bluepenguinconsulting.com>
2016-10-06 13:53:13 -0700

    Create bpc_video_background.php

---
+++ bpc_video_background.php
@@ -0,0 +1,156 @@
+<?php 
+/*
+Plugin Name: Fullscreen Background Video
+Plugin URI: http://bluepenguinconsulting.com
+Description: jQuery WordPress plugin to easily add a background video to an element
+Author: Julia H Chen
+Version: 1.0
+Author URI: http://bluepenguinconsulting.com
+*/
+
+/**
+ * Constants
+ */
+
+define( 'BPCFBV_VERSION', '1.0' );
+
+
+/**
+ * Store default settings in an array
+ */
+
+function bpcfbv_defaults() {
+
+	$defaults_array = array(
+
+		'id'                => 'hero_video',
+		'id_image'			=> 'hero_image',
+		'container'         => 'header',
+		'width'          	=> '16',
+		'height'       		=> '9',
+		'opt'       		=> '0',
+	);
+
+	return $defaults_array;
+}
+
+/**
+ * When plugin is installed, write default settings and update version
+ */
+
+function bpcfbv_install() {
+
+	$defaults_array = bpcfbv_defaults();
+	add_option( 'bpcfbv', $defaults_array );
+	update_option( 'bpcfbv_active_version', BPCFBV_VERSION );
+
+}
+register_activation_hook( __FILE__, 'bpcfbv_install' );
+
+
+
+/**
+ * If requested, when plugin is deactivated, remove settings
+ */
+
+function bpcfbv_uninstall() {
+	$settings = get_option( 'bpcfbv' );
+	if ( isset($settings['uninstall']) && $settings['uninstall'] ) {
+		delete_option( 'bpcfbv' );
+		delete_option( 'bpcfbv_active_version' );
+	}
+}
+register_deactivation_hook( __FILE__, 'bpcfbv_uninstall' );
+
+
+
+function bpc_video_background($id ='hero_video', $id_image='hero_image',  $container='header_vid', $width=16, $height=9, $opt=0) {
+
+if (is_front_page()) {
+	
+	
+
+?>
+
+<script>
+(function($) {
+
+		function fullscreen(id, id_image,  container, width, height, opt){
+		
+   			var videoDiv = document.getElementById(id);
+			var imageDiv = document.getElementById(id_image);
+			var containerDiv = document.getElementById(container);
+			var mediaAspect = height/width;	
+			var containerAspect = $(window).height() / $(window).width();
+			
+			if (/Mobi/.test(navigator.userAgent)) {
+    			// mobile -- show image
+				$(videoDiv).remove();
+				$(imageDiv).css('display', 'block');
+			}
+			else {
+		 	
+			if (containerAspect < mediaAspect) {
+				// too wide
+				
+				videoDiv.style.width = $(window).width() + "px";
+				videoDiv.style.height = ($(window).width() / (width / height)) + "px";
+				
+				$(containerDiv).css("height", ($(window).height()-opt));
+				
+				var marginch =  ($(window).width() * (height / width)) - $(window).height();
+				
+				if (marginch <0 ) {
+					videoDiv.style.left = 0;
+					videoDiv.style.top = 0;
+				}
+				else {
+					videoDiv.style.left = 0;
+					
+					videoDiv.style.top = "-"+ (marginch + opt) +"px";
+				}
+				
+			}
+			
+			else {
+				// too tall
+	
+				videoDiv.style.width = ($(window).height() * (width / height)) + "px";
+				
+				videoDiv.style.height = $(window).height() + "px";
+				$(containerDiv).css("height", ($(window).height()-opt));
+				
+				var marginch =  ($(window).height() * (width / height)) - $(window).width();
+	
+				
+				if (marginch <0 ) {
+					videoDiv.style.left = 0;
+					videoDiv.style.top = 0;
+				}
+				else {
+					videoDiv.style.top = "-"+opt+"px";
+					videoDiv.style.left = "-"+marginch+"px";
+				}
+			}
+			
+			}
+		
+	
+    }
+  
+    fullscreen("<?php echo $id; ?>", "<?php echo $id_image; ?>", "<?php echo $container; ?>", <?php echo $width; ?>, <?php echo $height; ?>, <?php echo $opt; ?>);
+
+  // Run the function in case of window resize
+  $(window).resize(function() {
+       fullscreen("<?php echo $id; ?>", "<?php echo $id_image; ?>", "<?php echo $container; ?>", <?php echo $width; ?>, <?php echo $height; ?>, <?php echo $opt; ?>); 
+	});        
+ 
+  })( jQuery );
+</script>
+<?php
+}}
+add_action( 'bpc_video_background', 'bpc_video_background' );
+
+
+
+?>
